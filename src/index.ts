607796293911be8fc0b97f7b3f96e9171658cc29
Robert Smieja <robertsmieja@users.noreply.github.com>
2020-05-27 02:23:47 -0400

    Add support for DiscordAPIToken to createClient()

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import { RobBotConfiguration } from "./configuration/types"
 import { RobBotClient } from "./client"
+import { createDefaultConfiguration } from "./configuration"
 
 export { createDefaultConfiguration } from "./configuration"
 
 export const createClient = (
-  configuration: RobBotConfiguration
+  configurationOrDiscordApiToken: RobBotConfiguration | string
 ): RobBotClient => {
+  if (typeof configurationOrDiscordApiToken === "string") {
+    const discordApiToken = configurationOrDiscordApiToken
+    return new RobBotClient(createDefaultConfiguration(discordApiToken))
+  }
+
+  const configuration: RobBotConfiguration = configurationOrDiscordApiToken
   return new RobBotClient(configuration)
 }
 
