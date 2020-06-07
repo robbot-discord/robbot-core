407ee3f9e921569f15c0e5786776ffe999c0290f
Robert Smieja <robertsmieja@users.noreply.github.com>
2020-06-07 12:26:14 -0400

    Fix LogLevelFilter middleware

--- src/logging/types.ts
+++ src/logging/types.ts
@@ -5,14 +5,23 @@ export type LogHandler = (message: LogMessage) => void
 /**
  * Lower is more critical, high is less critical.
  *
- * i.e. 'trace' is lowest, 'error' is highest
+ * i.e. 'trace' is highest, 'error' is lowest
  */
+
 export enum LogLevel {
   ERROR = "error",
   WARN = "warn",
   INFO = "info",
   DEBUG = "debug",
   TRACE = "trace",
+}
+
+export const LogLevelToNumber: Record<LogLevel, number> = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  debug: 3,
+  trace: 4,
 }
 
 export type Logger = Record<LogLevel, LogHandler>
--- src/middleware/logging/index.ts
+++ src/middleware/logging/index.ts
@@ -1,12 +1,13 @@
 import { LoggingMiddleware } from "../types"
 import { LogLevelFilterConfiguration } from "./types"
-import { LogLevel, Logger } from "../../logging/types"
+import { LogLevel, Logger, LogLevelToNumber } from "../../logging/types"
 
 export const createLoggingLevelFilter = (
   configuration: LogLevelFilterConfiguration
 ): LoggingMiddleware => {
   return (logger: Logger): Logger => {
     const { logLevel } = configuration
+    const logLevelNum = LogLevelToNumber[logLevel]
 
     const { ERROR, WARN, INFO, DEBUG, TRACE } = LogLevel
     const noOp = () => {
@@ -14,11 +15,26 @@ export const createLoggingLevelFilter = (
     }
 
     const wrappedLogger: Logger = {
-      error: logLevel >= ERROR ? (message) => logger.error(message) : noOp,
-      warn: logLevel >= WARN ? (message) => logger.warn(message) : noOp,
-      info: logLevel >= INFO ? (message) => logger.info(message) : noOp,
-      debug: logLevel >= DEBUG ? (message) => logger.debug(message) : noOp,
-      trace: logLevel >= TRACE ? (message) => logger.trace(message) : noOp,
+      error:
+        logLevelNum >= LogLevelToNumber[ERROR]
+          ? (message) => logger.error(message)
+          : noOp,
+      warn:
+        logLevelNum >= LogLevelToNumber[WARN]
+          ? (message) => logger.warn(message)
+          : noOp,
+      info:
+        logLevelNum >= LogLevelToNumber[INFO]
+          ? (message) => logger.info(message)
+          : noOp,
+      debug:
+        logLevelNum >= LogLevelToNumber[DEBUG]
+          ? (message) => logger.debug(message)
+          : noOp,
+      trace:
+        logLevelNum >= LogLevelToNumber[TRACE]
+          ? (message) => logger.trace(message)
+          : noOp,
     }
 
     return wrappedLogger
